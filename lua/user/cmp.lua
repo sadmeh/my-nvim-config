86df489a652d231c11ef945f003a8bdba73740e0
sadmeh <sadmeh@gmail.com>
2022-04-19 14:18:46 +0430

    fix document popup problem

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -119,11 +119,4 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
-  experimental = {
-    ghost_text = false,
-    native_menu = false,
-  },
 }
